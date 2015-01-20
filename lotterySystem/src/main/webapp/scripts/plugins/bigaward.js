fba065ce1939179c2675f7e09e88ae0e3b895c51
bolobeach <bolobeach@gmail.com>
2015-01-20 10:28:43 +0800

    提交标记

--- lotterySystem/src/main/webapp/scripts/plugins/bigaward.js
+++ lotterySystem/src/main/webapp/scripts/plugins/bigaward.js
@@ -5,6 +5,10 @@ $(document).ready(function(){
 	var longitude = null; // 经度
 	var latitude = null;// 这个是纬度
 	var tbody = $("#tbody");
+	var showAward =  $(".show-award");
+	showAward.find("div").on("click",function(){
+		showAward.hide();
+	});
 	tbody.find("tr").children("th").each(function(e){ // 行(经度)
 		var thNode = $(this);
 		 // 取消上次延时未执行的方法
@@ -18,8 +22,14 @@ $(document).ready(function(){
 			}
 			
 			if(latitude != null){ // 说明纬度已经被选择了,并且执行ajax
-				tbody.find("tr:gt(0):eq("+e+") td:eq("+latitude+")").css("background-color", "red");
-				$("#test").html("坐标为="+longitude+":"+latitude);
+				var bigawardNode = tbody.find("tr:eq("+e+") td:eq("+(latitude-1)+")");
+				bigawardNode.css("background-color", "red");
+				showAward.show();
+				showAward.find("div").css("background-color","#DEF3CA").html(bigawardNode.html()).animate({
+					width: "90%",
+					height: "90%",
+					opacity: 1
+				},2000);
 			}
 		});
 		
@@ -36,29 +46,38 @@ $(document).ready(function(){
 	
 	$("#trThead").find("th").each(function(e){ // 列(纬度)
 		var theadNode = $(this);
-		var index = e+1;
 		theadNode.on("click",function(){
 			// 取消上次延时未执行的方法
 		    clearTimeout(theadTime);
 		    //执行延时
 		    theadTime = setTimeout(function(){
 		    	latitude = null;
+		    	var index = e+1;
 				$("#tbody tr td:nth-child("+index+")").css("background-color","#DEF3CA");
+				
 		    },300);
 		});
 		
 		theadNode.on("dblclick",function(){
 			clearTimeout(theadTime); // 清楚事件
-			if(longitude != null){
-				alert(e);
-				tbody.find("tr:gt(0):eq("+longitude+") td:eq("+e+")").css("background-color", "red");
-				$("#test").html("thead:坐标为="+longitude+"--"+latitude);
-			}
 			if(latitude == null){
 				latitude = e;
+				var index = e+1;
+				
 				$("#tbody tr td:nth-child("+index+")").css("background-color","#9BAF83");
 			}
 			
+			if(longitude != null){ // this is write ajax
+				var index = e-1;
+				var bigawardNode =tbody.find("tr:eq("+longitude+") td:eq("+index+")") ;
+				bigawardNode.css("background-color", "red");
+				showAward.show();
+				showAward.find("div").css("background-color","#DEF3CA").html(bigawardNode.html()).animate({
+					width: "90%",
+					height: "90%",
+					opacity: 1
+				},2000);
+			}
 		});
 		
 		
